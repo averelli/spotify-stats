5e7ce8a782c8ef3b3ea2dab26570fe4b0d14d8e5
averelli <beloglazov.roman06@gmail.com>
2024-08-09 12:51:27 +0300

    Create __init__.py files for all the modules in the project. Add a db uri in the config __init__.py

---
+++ database/__init__.py
@@ -0,0 +1,2 @@
+from .connect import get_db
+from .operations import save_top_tracks
---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from .auth import authenticate_spotify
+from .fetch import fetch_top_tracks
+from .process_data import porocess_tracks
