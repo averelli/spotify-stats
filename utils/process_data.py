b1bcc12a7df4f2ba64b1c2ee6c00c16a96f77e54
averelli <beloglazov.roman06@gmail.com>
2024-08-12 12:42:28 +0300

    Add process_top_artists() function

--- utils/process_data.py
+++ utils/process_data.py
@@ -20,7 +20,7 @@ def process_top_tracks(sp: Spotify, top_tracks: dict) -> list:
     try:
         for term, tracks in top_tracks.items():
 
-            track_ids = [track['id'] for track in tracks['items']]
+            track_ids = [track["id"] for track in tracks["items"]]
             audio_features_list = fetch_audio_features(sp, track_ids)
 
             term_data = {
@@ -31,24 +31,24 @@ def process_top_tracks(sp: Spotify, top_tracks: dict) -> list:
             # track position in the chart, 1 is highest
             track_position = 1
 
-            for track, audio_features in zip(tracks['items'], audio_features_list):
+            for track, audio_features in zip(tracks["items"], audio_features_list):
                 track_info = {
-                    "track_id": track['id'],
-                    "title": track['name'],
-                    "album_id": track['album']['id'],
-                    "album_name": track['album']['name'],
-                    "artist_id": track['artists'][0]['id'],
-                    "artist_name": track['artists'][0]['name'],
-                    "release_date": track['album']['release_date'],
-                    "cover_art_url": track['album']['images'][0]['url'] if track['album']['images'] else None,
-                    "duration_ms": track['duration_ms'],
-                    "popularity": track['popularity'],
-                    "uri": track['uri'],
+                    "track_id": track["id"],
+                    "title": track["name"],
+                    "album_id": track["album"]["id"],
+                    "album_name": track["album"]["name"],
+                    "artist_id": track["artists"][0]["id"],
+                    "artist_name": track["artists"][0]["name"],
+                    "release_date": track["album"]["release_date"],
+                    "cover_art_url": track["album"]["images"][0]["url"] if track["album"]["images"] else None,
+                    "duration_ms": track["duration_ms"],
+                    "popularity": track["popularity"],
+                    "uri": track["uri"],
                     "audio_features": {
-                        "danceability": audio_features['danceability'],
-                        "energy": audio_features['energy'],
-                        "valence": audio_features['valence'],
-                        "tempo": audio_features['tempo']
+                        "danceability": audio_features["danceability"],
+                        "energy": audio_features["energy"],
+                        "valence": audio_features["valence"],
+                        "tempo": audio_features["tempo"]
                     },
                     "chart_position": track_position
                 }
@@ -65,3 +65,45 @@ def process_top_tracks(sp: Spotify, top_tracks: dict) -> list:
     
     return processed_tracks
 
+def process_top_artists(top_artists: dict) -> list:
+    """
+    Processes top artists data to extract relevant information.
+
+    Args:
+        top_artists (dict): A dictionary of top artists data returned from fetch_top_artists()
+
+    Returns:
+        list: A list of dictionaries, each containing the processed artist information.
+    """
+    processed_artists = []
+    
+    try:
+        for time_frame, artists in top_artists.items():
+            time_frame_data = {
+                "time_frame": time_frame,
+                "artists_data": []
+            }
+
+            artist_position = 1
+            
+            # Process each artist in the time frame
+            for artist in artists["items"]:
+                artist_info = {
+                    "name": artist["name"],
+                    "genres": artist["genres"],
+                    "followers": artist["followers"]["total"],
+                    "uri": artist["uri"],
+                    "image_url": artist["images"][0]["url"] if artist["images"] else None,
+                    "chart_postion": artist_position
+                }
+                time_frame_data["artists_data"].append(artist_info)
+            
+            processed_artists.append(time_frame_data)
+            artist_position += 1
+            
+        logger.info("Top artists processed successfully.")
+    
+    except Exception as e:
+        logger.error(f"Error processing top artists: {e}")
+    
+    return processed_artists
