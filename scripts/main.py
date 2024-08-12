c3680874b69f816b2a39647be9d304b833902d58
averelli <beloglazov.roman06@gmail.com>
2024-08-12 12:43:28 +0300

    Add saving top artists

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,5 @@
-from utils import authenticate_spotify, fetch_top_tracks, process_top_tracks
-from database import save_top_tracks
+from utils import authenticate_spotify, fetch_top_tracks, process_top_tracks, fetch_top_artists, process_top_artists
+from database import save_top_tracks, save_top_artists
 from logs import logger
 scope = "user-top-read"
 logger.name = "Main"
@@ -17,14 +17,18 @@ def main():
         logger.error(f"Error authenticating with Spotify: {e}")
         return
 
-    # Fetch top tracks
+    # Fetch top tracks and artists
     top_tracks = fetch_top_tracks(sp, limit=50)
+    top_artists = fetch_top_artists(sp, limit=50)
+
     
     # Process and save each time frame individually
     processed_tracks = process_top_tracks(sp, top_tracks)
+    processed_artists = process_top_artists(top_artists)
 
-    for time_frame in range(len(processed_tracks)):
+    for time_frame in range(3):
         save_top_tracks(processed_tracks[time_frame])
+        save_top_artists(processed_artists[time_frame])
 
 
 if __name__ == '__main__':
