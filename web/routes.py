c4f749a67eff245319749fd2aaed8f2b6d6ea544
averelli <beloglazov.roman06@gmail.com>
2024-08-28 12:31:25 +0300

    Add chart type arg to top_tracks, and some cosmetic changes

--- web/routes.py
+++ web/routes.py
@@ -17,6 +17,7 @@ def search():
     chart_type = request.form.get("type")
     time_frame = request.form.get("range")
     days_ago = request.form.get("days")
+
     return redirect(url_for("main.compare", chart_type=chart_type, time_frame=time_frame, days_ago=int(days_ago)))
 
 @main_bp.route("/top_tracks")
@@ -24,15 +25,17 @@ def top_tracks():
     short_term_chart = fetch_chart_document("tracks", "short_term")["tracks_data"]
     medium_term_chart = fetch_chart_document("tracks", "medium_term")["tracks_data"]
     long_term_chart = fetch_chart_document("tracks", "long_term")["tracks_data"]
-    print(short_term_chart)
+
     return render_template("top_tracks.html", 
                            short_term_chart=short_term_chart, 
                            medium_term_chart=medium_term_chart, 
-                           long_term_chart=long_term_chart)
+                           long_term_chart=long_term_chart,
+                           chart_type = "tracks")
 
 @main_bp.route("/compare/<chart_type>/<time_frame>/<int:days_ago>")
 def compare(chart_type, time_frame, days_ago):
     comparison_data = compare_charts(chart_type, time_frame, days_ago)
+
     return render_template("compare.html", 
                            title=f"Comparison for {chart_type.capitalize()} - {time_frame.replace('_', ' ').capitalize()} ({days_ago} days ago):",
                            chart=comparison_data["chart"])
