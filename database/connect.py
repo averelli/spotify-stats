5e55b3ae807fc22de40e062bcf7f485da365beff
averelli <beloglazov.roman06@gmail.com>
2024-08-28 14:04:41 +0300

    Redo the connection function to not create a new connection every time

--- database/connect.py
+++ database/connect.py
@@ -1,12 +1,16 @@
 from pymongo import MongoClient
 from config import MONGO_URI
 
+db = None
+
 def get_db():
     """
     Establishes a connection to the MongoDB cluster and returns the database instance.
     Returns:
         db (Database): The MongoDB database instance.
     """
-    client = MongoClient(MONGO_URI)
-    db = client["spotify_data"]
+    global db
+    if db is None:
+        client = MongoClient(MONGO_URI)
+        db = client["spotify_data"]
     return db
