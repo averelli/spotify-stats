840ec6ef50163472efa5059c68320af4125756a5
averelli <beloglazov.roman06@gmail.com>
2024-08-28 20:42:14 +0300

    Minor change to use only one Spotify object

--- utils/auth.py
+++ utils/auth.py
@@ -3,6 +3,8 @@ from spotipy.oauth2 import SpotifyOAuth
 import config
 from config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
+sp = None
+
 def authenticate_spotify(scope: str) -> spotipy.client.Spotify:
     """
         Handles authentication based on the scope provided.
@@ -11,9 +13,11 @@ def authenticate_spotify(scope: str) -> spotipy.client.Spotify:
         Returns:
             sp(spotipy.client.Spotify): Spotify client
     """
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                                   client_secret=CLIENT_SECRET,
-                                                   redirect_uri=REDIRECT_URI,
-                                                   scope=scope,
-                                                   cache_path="config/.cache"))
+    global sp
+    if sp is None:
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                                    client_secret=CLIENT_SECRET,
+                                                    redirect_uri=REDIRECT_URI,
+                                                    scope=scope,
+                                                    cache_path="config/.cache"))
     return sp
