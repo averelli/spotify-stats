e1934ade197a9e5180a468e7bc6bb58c3fc64fa0
averelli <beloglazov.roman06@gmail.com>
2024-08-28 20:40:57 +0300

    Add get track/artist's doc

--- database/operations.py
+++ database/operations.py
@@ -3,8 +3,6 @@ from database import get_db
 from logs import logger
 from datetime import timedelta
 
-logger.name = "DB-OPERATIONS"
-
 def save_top_tracks(data: dict):
     """
     Saves the top tracks to the database with the specified time period.
@@ -82,3 +80,44 @@ def fetch_chart_document(chart_type:str, time_frame: str, days_ago: int = 0) -> 
         logger.error(f"Error fetching tracks for time frame {time_frame}: {e}")
         return None
     
+
+def get_track_details(track_id):
+    """
+    Retrieves the details of a specific track from the top_tracks collection.
+    Args:
+        track_id (str): The unique identifier for the track.
+    Returns:
+        dict: A dictionary containing the track details (title, artist, album, etc.)
+              if the track is found. If no track is found, returns None.
+    """
+    db = get_db()
+    # Find the first document where the track is present
+    track_doc = db.top_tracks.find_one({"tracks_data.track_id": track_id}, {"tracks_data.$": 1})
+    if track_doc and "tracks_data" in track_doc:
+        return track_doc["tracks_data"][0]  # Return the first matching track
+    return None
+
+def get_artist_details(artist_id):
+    """
+    Retrieves the details of a specific artist from the top_tracks collection.
+    Args:
+        artist_id (str): The unique identifier for the artist.
+    Returns:
+        dict: A dictionary containing the artist details (artist name, tracks, etc.)
+              if the artist is found. If no artist is found, returns None.
+    """
+    db = get_db()
+    artist_result = db.top_artists.aggregate([
+        {"$unwind": "$artists_data"},
+        {"$match": {"artists_data.artist_id": artist_id}},
+        {"$sort": {"timestamp": -1}},
+        {"$limit": 1},
+        {"$project": {
+            "_id": 0,
+            "artists_data": 1
+        }}
+    ])
+    artist_doc = next(artist_result, None)
+    if artist_doc:
+        return artist_doc["artists_data"]  # Return the first matching artist data
+    return None
