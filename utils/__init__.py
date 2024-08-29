368667c7f46b62fde05c20b2b3ff36de06a93b69
averelli <beloglazov.roman06@gmail.com>
2024-08-29 13:34:22 +0300

    Add new functions

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .auth import authenticate_spotify
-from .fetch import fetch_top_tracks, fetch_audio_features, fetch_top_artists
+from .fetch import fetch_top_tracks, fetch_audio_features, fetch_top_artists, fetch_artist_details
 from .process_data import process_top_tracks, process_top_artists
