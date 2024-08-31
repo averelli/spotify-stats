96e335cb26e373c4db612c941b78ce607a66fcc2
averelli <beloglazov.roman06@gmail.com>
2024-08-31 11:16:24 +0300

    Add top artists route

--- web/routes.py
+++ web/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, abort
 from scripts.compare import compare_charts
 from database import fetch_chart_document, get_track_details, get_artist_details
 from utils import fetch_artist_details
-from stats import get_track_chart_stats
+from stats import get_track_chart_stats, get_artist_chart_stats
 
 main_bp = Blueprint("main", __name__)
 
@@ -28,20 +28,32 @@ def top_tracks():
     medium_term_chart = fetch_chart_document("tracks", "medium_term")["tracks_data"]
     long_term_chart = fetch_chart_document("tracks", "long_term")["tracks_data"]
 
-    return render_template("top_tracks.html", 
+    return render_template("top_chart.html", 
                            short_term_chart=short_term_chart, 
                            medium_term_chart=medium_term_chart, 
                            long_term_chart=long_term_chart,
                            chart_type = "tracks")
 
+@main_bp.route("/top_artists")
+def top_artists():
+    short_term_chart = fetch_chart_document("artists", "short_term")["artists_data"]
+    medium_term_chart = fetch_chart_document("artists", "medium_term")["artists_data"]
+    long_term_chart = fetch_chart_document("artists", "long_term")["artists_data"]
+
+    return render_template("top_chart.html",
+                           short_term_chart=short_term_chart, 
+                           medium_term_chart=medium_term_chart, 
+                           long_term_chart=long_term_chart,
+                           chart_type = "artists")
+
 @main_bp.route("/compare/<chart_type>/<time_frame>/<int:days_ago>")
 def compare(chart_type, time_frame, days_ago):
     comparison_data = compare_charts(chart_type, time_frame, days_ago)
-
+    print(comparison_data)
     return render_template("compare.html", 
                            title=f"Comparison for {chart_type.capitalize()} - {time_frame.replace('_', ' ').capitalize()} ({days_ago} days ago):",
                            chart=comparison_data["chart"],
-                           chart_type = "tracks")
+                           chart_type = chart_type)
 
 @main_bp.route("/tracks/<track_id>")
 def track(track_id):
@@ -64,4 +76,7 @@ def artist(artist_id):
     if artist_data is None:
         abort(404)
 
-    return render_template("card_display.html", artist_data = artist_data, page_type="artist_card.html", title=artist_data["name"])
+    user_stats = get_artist_chart_stats(artist_id)
+    print(user_stats)
+
+    return render_template("card_display.html", artist_data = artist_data, stats=user_stats, page_type="artist_card.html", title=artist_data["name"])
