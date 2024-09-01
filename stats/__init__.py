09e83244336271a62dc38ac2291daed265e484a2
averelli <beloglazov.roman06@gmail.com>
2024-09-01 20:04:53 +0300

    Add getting artist's stats

--- stats/__init__.py
+++ stats/__init__.py
@@ -1,1 +1,1 @@
-from .track_stats import get_track_chart_stats
+from .item_stats import get_track_chart_stats, get_artist_chart_stats
--- stats/item_stats.py
+++ stats/item_stats.py
@@ -1,13 +1,9 @@
 from database import get_db
 
-
-
-# Connect to MongoDB
-db = get_db()
-collection = db["top_tracks"]
-
-# Function to get chart statistics for a specific track
 def get_track_chart_stats(track_id):
+    # Connect to MongoDB
+    db = get_db()
+    collection = db["top_tracks"]
     pipeline = [
         # Match documents containing the specified track_id
         {"$match": {"tracks_data.track_id": track_id}},
@@ -55,3 +51,42 @@ def get_track_chart_stats(track_id):
     
     return track_stats
 
+def get_artist_chart_stats(artist_id):
+    db  = get_db()
+    collection = db["top_artists"]
+    pipeline = [
+        {"$match": {"artists_data.artist_id": artist_id}},
+
+        {"$unwind": "$artists_data"},
+
+        {"$match": {"artists_data.artist_id": artist_id}},
+
+        {"$sort": {"timestamp": -1}},
+
+        {
+            "$group": {
+                "_id": {
+                    "artist_id": "$artists_data.artist_id",
+                    "time_frame": "$time_frame"
+                },
+                "max_position": {"$min": "$artists_data.chart_position"},
+                "min_position": {"$max": "$artists_data.chart_position"},
+                "duration": {"$sum": 1},
+                "latest_chart_position": {"$first": "$artists_data.chart_position"}
+            }
+        },
+
+        {"$sort": {"_id.time_frame": 1}}
+    ]
+    results = list(collection.aggregate(pipeline))
+    artist_stats = []
+    for result in results:
+        artist_stats.append({
+            "time_frame": result["_id"]["time_frame"],
+            "max_position": result["max_position"],
+            "min_position": result["min_position"],
+            "duration": result["duration"],
+            "chart_position": result["latest_chart_position"]
+        })
+
+    return artist_stats
